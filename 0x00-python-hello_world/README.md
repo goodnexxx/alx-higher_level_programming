9353e07bfe1f46b187ea24b0d4035432887e9ab3
goodness <goodnessadaokun@gmail.com>
2022-09-22 20:46:19 +0000

    d

---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+## Becoming Familiar with Python
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    low = ord(c)
+    if low >= 97 and low <= 122:
+        return True
+    else:
+        return False
